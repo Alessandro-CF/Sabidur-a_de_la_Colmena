78541686b7e10201428df84b954803c9eb9556e3
Alessandro-CF <achoque.231ds12@istta.edu.pe>
2025-05-05 11:50:58 -0500

    Creación del Front-End Home sin funcionalidad

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,14 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/', function () {
+    return Inertia::render('Home/Home');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
